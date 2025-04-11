5ddd4a9c0968052435031711a44b1564dc4543af
LouisZCode <lgzg90@hotmail.com>
2025-04-11 17:45:44 +0200

    Finish Chains Lesson

--- module-1/ex_2_chains.py
+++ module-1/ex_2_chains.py
@@ -1,17 +1,73 @@
 from pprint import pprint
+from dotenv import load_dotenv
 from langchain_core.messages import AIMessage, HumanMessage
+from langchain_anthropic import ChatAnthropic
+from typing import TypedDict
+#This seems to be  a placeholder for... you guessed it... any message
+from langchain_core.messages import AnyMessage
+#Lets also get the important modules so we can use Reducer and not only overwrite our messages:
+from typing import Annotated
+from langgraph.graph.message import add_messages
+#Lets get what we need to create the Graph
+from IPython.display import Image, display
+from langgraph.graph import StateGraph, START, END
 
-from langchain_ollama import OllamaLLM
 
-llm = OllamaLLM(model="llama3.1:8b")
-
-llm.invoke("The first man on the moon was ...")
+# Load environment variables from .env file
+load_dotenv()
 
 
-messages = [AIMessage(content=f"So you said you were researching ocean mammals?", name="Model")]
-messages.append(HumanMessage(content=f"Yes, that's right.",name="Lance"))
-messages.append(AIMessage(content=f"Great, what would you like to learn about.", name="Model"))
-messages.append(HumanMessage(content=f"I want to learn about the best place to see Orcas in the US.", name="Lance"))
+# Using Claude
+llm = ChatAnthropic(
+    model="claude-3-sonnet-20240229",
+    temperature=0
+)
 
-for m in messages:
+human_says = input("You:\n")
+
+
+#NOTE STATE
+#Here we create the state
+class MessageState(TypedDict):
+    #...and also we add a reducer so to not overwrite all our messages.
+    messages: Annotated[list[AnyMessage], add_messages]
+
+
+#NOTE NODE
+def tool_calling_llm(state: MessageState):
+    return {"messages": [llm_with_tools.invoke(state["messages"])]}
+
+
+
+#NOTE TOOL
+# Define a  tool
+def multiply(a: int, b: int) -> int:
+    """Multiply a and b.
+
+    Args:
+        a: first int
+        b: second int
+    """
+    return a * b
+
+
+# Bind the tool to the LLM
+llm_with_tools = llm.bind_tools([multiply])
+
+#NOTE Build the Graph:
+builder = StateGraph(MessageState)
+builder.add_node("tool_calling_llm", tool_calling_llm)
+builder.add_edge(START, "tool_calling_llm")
+builder.add_edge("tool_calling_llm", END)
+graph = builder.compile()
+
+# View
+display(Image(graph.get_graph().draw_mermaid_png()))
+
+#Here we have the original message
+messages = graph.invoke({"messages": HumanMessage(content=f"{human_says}", name="Luis")})
+for m in messages['messages']:
     m.pretty_print()
+
+
+
